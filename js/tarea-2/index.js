0a85a064a633dda0468278f1856ee90a07b55050
SuperJav <javielito2405@hotmail.com>
2022-12-19 11:25:43 -0400

    Tarea-2

---
+++ js/tarea-2/index.js
@@ -0,0 +1,17 @@
+ let Nombre= "Javielito Ramirez Brioso.";
+ const Edad= "22";
+ let EresDesarrollador= true;
+ let FechaNacimineto= new Date(2000,5,24)
+const libroFaborito={
+    titulo: "Type Script",
+    autor:"Emmanuel Valverde Ramos.",
+    fecha: new Date(2016,09,21),
+    URL:"https://openlibra.com/es/book/typescript"
+}
+
+ console.log(FechaNacimineto);
+ console.log(Edad);
+ console.log(libroFaborito);
+ console.log(Nombre);
+
+ 
